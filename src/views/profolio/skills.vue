90e954f7f5e107f4bad9d9167b7255bc098b9779
wytang1234 <yu-v-@hotmail.com>
2020-12-21 23:01:06 +0800

    skills

--- src/views/profolio/skills.vue
+++ src/views/profolio/skills.vue
@@ -141,6 +141,8 @@
               </md-card>
             </div>
           </div>
+        </div>
+        <div class="md-layout">
           <div class="md-layout-item md-medium-size-33 md-small-size-100">
             <div class="team-player">
               <md-card class="md-card-plain">
@@ -153,6 +155,94 @@
                 </div>
                 <h4 class="card-title">
                   Python
+                  <br />
+                  <small class="card-description text-muted"
+                    >Backend</small
+                  >
+                </h4>
+
+                <md-card-content>
+                  <p class="card-description">
+                    You can write here details about one of your team
+                    members. You can give more details about what they do.
+                    Feel free to add some <a href="#">links</a> for people
+                    to be able to follow them outside the site.
+                  </p>
+                </md-card-content>
+
+                <md-card-actions class="text-center">
+                  <md-button
+                    href="javascript:void(0)"
+                    class="md-just-icon md-simple"
+                  >
+                    <i class="fab fa-twitter"></i>
+                  </md-button>
+                  <md-button
+                    href="javascript:void(0)"
+                    class="md-just-icon md-simple"
+                  >
+                    <i class="fab fa-linkedin"></i>
+                  </md-button>
+                </md-card-actions>
+              </md-card>
+            </div>
+          </div>
+          <div class="md-layout-item md-medium-size-33 md-small-size-100">
+            <div class="team-player">
+              <md-card class="md-card-plain">
+                <div class="md-layout-item md-size-50 mx-auto">
+                  <img
+                    :src="arduino.image"
+                    alt="Thumbnail Image"
+                    class="img-raised rounded-circle img-fluid"
+                  />
+                </div>
+                <h4 class="card-title">
+                  Arduino
+                  <br />
+                  <small class="card-description text-muted"
+                    >IOT</small
+                  >
+                </h4>
+
+                <md-card-content>
+                  <p class="card-description">
+                    You can write here details about one of your team
+                    members. You can give more details about what they do.
+                    Feel free to add some <a href="#">links</a> for people
+                    to be able to follow them outside the site.
+                  </p>
+                </md-card-content>
+
+                <md-card-actions class="text-center">
+                  <md-button
+                    href="javascript:void(0)"
+                    class="md-just-icon md-simple"
+                  >
+                    <i class="fab fa-twitter"></i>
+                  </md-button>
+                  <md-button
+                    href="javascript:void(0)"
+                    class="md-just-icon md-simple"
+                  >
+                    <i class="fab fa-linkedin"></i>
+                  </md-button>
+                </md-card-actions>
+              </md-card>
+            </div>
+          </div>
+          <div class="md-layout-item md-medium-size-33 md-small-size-100">
+            <div class="team-player">
+              <md-card class="md-card-plain">
+                <div class="md-layout-item md-size-50 mx-auto">
+                  <img
+                    :src="go.image"
+                    alt="Thumbnail Image"
+                    class="img-raised rounded-circle img-fluid"
+                  />
+                </div>
+                <h4 class="card-title">
+                  Go
                   <br />
                   <small class="card-description text-muted"
                     >Backend</small
@@ -206,6 +296,12 @@
         },
         python: {
           image: require("@/assets/img/skills/python.png")
+        },
+        arduino: {
+          image: require("@/assets/img/skills/arduino.png")
+        },
+        go: {
+          image: require("@/assets/img/skills/go.png")
         }
       }
     }
