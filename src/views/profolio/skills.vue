6160bc3528c137818400e172fa5bccd19177c9a8
wytang1234 <yu-v-@hotmail.com>
2020-12-22 03:00:31 +0800

    finished major minor badge

--- src/views/profolio/skills.vue
+++ src/views/profolio/skills.vue
@@ -2,6 +2,7 @@
   <div>
     <hr>
     <h2 class="title text-center" id="skills">Skills</h2>
+    <div class="text-center">Feel free to select tags label ;)</div>
     <div class="section text-center">
       <div class="team">
         <div class="md-layout">
@@ -19,22 +20,17 @@
                   {{nodejs.name}}
                   <br />
                   <small class="card-description text-muted"
-                    >Backend</small
+                    >{{nodejs.description}}</small
                   >
                 </h4>
-                <md-card-content>
-                  <badge type="default">Admin Panel</badge>
-                  <badge type="default">Socket I/O</badge>
-                  <badge type="default">Express</badge>
-                  <badge type="default">Fastify</badge>
-                  <badge type="default">Firebase</badge>
-                  <badge type="default">Blockchain</badge>
-                  <badge type="default">ETH Contract</badge>
-                  <badge type="default">Telegram Bot</badge>
-                  <badge type="default">Axios</badge>
-                  <badge type="default">Curl</badge>
-                  <badge type="default">Cron Job</badge>
-                </md-card-content>
+                <div
+                  v-for="tag in nodejs.tags" 
+                  :key="tag.id"
+                  @click="clicked_tag(tag)"
+                  :class="selected_tag(tag)"
+                >
+                  {{tag.name}}
+                </div>
               </md-card>
             </div>
           </div>
@@ -52,20 +48,17 @@
                   {{aws.name}}
                   <br />
                   <small class="card-description text-muted"
-                    >Cloud Service</small
+                    >{{aws.description}}</small
                   >
                 </h4>
-                <md-card-content>
-                  <badge type="default">EC2</badge>
-                  <badge type="default">S3</badge>
-                  <badge type="default">DynamoDB</badge>
-                  <badge type="default">RDS</badge>
-                  <badge type="default">Lambda</badge>
-                  <badge type="default">SNS</badge>
-                  <badge type="default">IoT Core</badge>
-                  <badge type="default">API Gateway</badge>
-                  <badge type="default">IAM</badge>
-                </md-card-content>
+                <div
+                  v-for="tag in aws.tags" 
+                  :key="tag.id"
+                  @click="clicked_tag(tag)"
+                  :class="selected_tag(tag)"
+                >
+                  {{tag.name}}
+                </div>
               </md-card>
             </div>
           </div>
@@ -83,16 +76,17 @@
                   {{arduino.name}}
                   <br />
                   <small class="card-description text-muted"
-                    >IOT</small
+                    >{{arduino.description}}</small
                   >
                 </h4>
-                <md-card-content>
-                  <badge type="default">WiFi ESP8266</badge>
-                  <badge type="default">Big Data IoT</badge>
-                  <badge type="default">Robot Car</badge>
-                  <badge type="default">Home Security</badge>
-                  <badge type="default">Music Box</badge>
-                </md-card-content>
+                <div
+                  v-for="tag in arduino.tags" 
+                  :key="tag.id"
+                  @click="clicked_tag(tag)"
+                  :class="selected_tag(tag)"
+                >
+                  {{tag.name}}
+                </div>
               </md-card>
             </div>
           </div>
@@ -110,16 +104,17 @@
                   {{python.name}}
                   <br />
                   <small class="card-description text-muted"
-                    >Backend</small
+                    >{{python.description}}</small
                   >
                 </h4>
-
-                <md-card-content>
-                  <badge type="default">System Control Script</badge>
-                  <badge type="default">Curl</badge>
-                  <badge type="default">Face Recognition</badge>
-                  <badge type="default">Big Data</badge>
-                </md-card-content>
+                <div
+                  v-for="tag in python.tags" 
+                  :key="tag.id"
+                  @click="clicked_tag(tag)"
+                  :class="selected_tag(tag)"
+                >
+                  {{tag.name}}
+                </div>
               </md-card>
             </div>
           </div>
@@ -136,14 +131,16 @@
                 <h4 class="card-title">
                   {{vue.name}}
                   <br />
-                  <small class="card-description text-muted">Frontend</small>
+                  <small class="card-description text-muted">{{vue.description}}</small>
                 </h4>
-
-                <md-card-content>
-                  <badge type="default">Admin Panel</badge>
-                  <badge type="default">Portfolio</badge>
-                  <badge type="default">vue-cli-service</badge>
-                </md-card-content>
+                <div
+                  v-for="tag in vue.tags" 
+                  :key="tag.id"
+                  @click="clicked_tag(tag)"
+                  :class="selected_tag(tag)"
+                >
+                  {{tag.name}}
+                </div>
               </md-card>
             </div>
           </div>
@@ -161,7 +158,12 @@
                 </div>
                 
                 <h4 class="card-title">
-                  <badge type="default">{{go.name}}</badge>
+                  <div
+                    @click="clicked_tag(go)"
+                    :class="selected_tag(go)"
+                  >
+                    {{go.name}}
+                  </div>
                   <br />
                 </h4>
               </md-card>
@@ -179,7 +181,12 @@
                 </div>
                 
                 <h4 class="card-title">
-                  <badge type="default">{{expo.name}}</badge>
+                  <div
+                    @click="clicked_tag(expo)"
+                    :class="selected_tag(expo)"
+                  >
+                    {{expo.name}}
+                  </div>
                   <br />
                 </h4>
               </md-card>
@@ -197,7 +204,12 @@
                 </div>
                 
                 <h4 class="card-title">
-                  <badge type="default">{{processing.name}}</badge>
+                  <div
+                    @click="clicked_tag(processing)"
+                    :class="selected_tag(processing)"
+                  >
+                    {{processing.name}}
+                  </div>
                   <br />
                 </h4>
               </md-card>
@@ -210,45 +222,106 @@
 </template>
 
 <script>
-  import { Badge } from '@/components'
   export default {
-    components: {
-      Badge
-    },
     data() {
+      let vue = {
+        image: require("@/assets/img/skills/vue.png"),
+        name: "Vue",
+        description: "Frontend",
+        tags: [
+          { id: "vue_admin", name: "Admin Panel", clicked: false },
+          { id: "vue_portfolio", name: "Portfolio", clicked: false },
+          { id: "vue_cli_service", name: "vue-cli-service", clicked: false },
+          { id: "vue_material", name: "Material", clicked: false }
+        ] 
+      }
+      let nodejs = {
+        image: require("@/assets/img/skills/nodejs.png"),
+        name: "Nodejs",
+        description: "Backend",
+        tags: [
+          { id: "node_admin", name: "Admin Panel", clicked: false },
+          { id: "node_socket", name: "socket I/O", clicked: false },
+          { id: "node_express", name: "Express", clicked: false },
+          { id: "node_fastify", name: "Fastify", clicked: false },
+          { id: "node_firebase", name: "Firebase", clicked: false },
+          { id: "node_blockchain", name: "Blockchain", clicked: false },
+          { id: "node_eth", name: "ETH contract", clicked: false },
+          { id: "node_telegram", name: "Telegram Bot", clicked: false },
+          { id: "node_axios", name: "axios", clicked: false },
+          { id: "node_curl", name: "curl", clicked: false },
+          { id: "node_cron", name: "Cron Job", clicked: false }
+        ]
+      }
+      let aws = {
+        image: require("@/assets/img/skills/aws.png"),
+        name: "Amazon Web Services",
+        description: "Cloud Services",
+        tags: [ 
+          { id:"aws_ec2", name: "EC2", clicked: false },
+          { id:"aws_s3", name: "S3", clicked: false },
+          { id:"aws_dynamodb", name: "DynamoDB", clicked: false },
+          { id:"aws_rds", name: "RDS", clicked: false },
+          { id:"aws_lambda", name: "Lambda", clicked: false },
+          { id:"aws_sns", name: "SNS", clicked: false },
+          { id:"aws_iot", name: "IoT Core", clicked: false },
+          { id:"aws_apigateway", name: "API Gateway", clicked: false },
+          { id:"aws_iam", name: "IAM", clicked: false }
+        ]
+      }
+      let python = {
+        image: require("@/assets/img/skills/python.png"),
+        name: "Python",
+        description: "Backend",
+        tags: [
+          { id: "py_system", name: "System Control", clicked: false },
+          { id: "py_curl", name: "curl", clicked: false },
+          { id: "py_face", name: "Face Recognition", clicked: false },
+          { id: "py_bigdata", name: "Big Data", clicked: false },
+        ]
+      }
+      let arduino = {
+        image: require("@/assets/img/skills/arduino.png"),
+        name: "Arduino",
+        description: "IOT",
+        tags: [
+          { id: "arduino_esp8266", name: "WIFI ESP8266", clicked: false },
+          { id: "arduino_bigdata", name: "Big Data", clicked: false },
+          { id: "arduino_robot", name: "Robot Car", clicked: false },
+          { id: "arduino_home", name: "Home security", clicked: false },
+          { id: "arduino_music", name: "Music Box", clicked: false },
+        ] 
+      }
+      let go = {
+        image: require("@/assets/img/skills/go.png"),
+        name: "Go",
+        clicked: false
+      }
+      let expo = {
+        image: require("@/assets/img/skills/expo.png"),
+        name: "Expo",
+        clicked: false
+      }
+      let processing = {
+        image: require("@/assets/img/skills/processing.png"),
+        name: "Processing",
+        clicked: false
+      }
       return {
-        vue: {
-          image: require("@/assets/img/skills/vue.png"),
-          name: "Vue"
-        },
-        nodejs: {
-          image: require("@/assets/img/skills/nodejs.png"),
-          name: "Nodejs"
-        },
-        aws: {
-          image: require("@/assets/img/skills/aws.png"),
-          name: "Amazon Web Services"
-        },
-        python: {
-          image: require("@/assets/img/skills/python.png"),
-          name: "Python"
-        },
-        arduino: {
-          image: require("@/assets/img/skills/arduino.png"),
-          name: "Arduino"
-        },
-        go: {
-          image: require("@/assets/img/skills/go.png"),
-          name: "Go"
-        },
-        expo: {
-          image: require("@/assets/img/skills/expo.png"),
-          name: "Expo"
-        },
-        processing: {
-          image: require("@/assets/img/skills/processing.png"),
-          name: "Processing"
-        }
+        vue, nodejs, aws, python, arduino, go, expo, processing,
+        major_skills: [ nodejs, aws, arduino, python, vue ],
+        minor_skills: [ go, expo, processing ]
+      }
+    },
+    methods: {
+      selected_tag(tag){
+        if(tag && tag.clicked)
+          return "badge badge-success"
+        else
+          return "badge badge-default"
+      },
+      clicked_tag(tag){
+        tag.clicked = !tag.clicked
       }
     }
   };
