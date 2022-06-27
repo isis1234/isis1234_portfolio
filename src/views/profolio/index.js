8bd026488ec8596b99ee18c80cff7a9b16a33c7b
wytang1234 <yu-v-@hotmail.com>
2022-06-28 03:14:36 +0800

    remove useless

--- src/views/profolio/index.js
+++
@@ -1,6 +0,0 @@
-import Skills from "./skills.vue";
-import Experiences from "./experiences.vue";
-import Educations from "./educations.vue";
-import Projects from "./projects.vue";
-
-export { Skills, Experiences, Educations, Projects };
